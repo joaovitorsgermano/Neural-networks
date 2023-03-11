f8e8da5e42c9a25cc11b79ec246187fa81b4044d
Germano <Joao.Germano@saint-gobain.com>
2023-03-11 00:00:05 -0300

    first commit

---
+++ rede.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def importDocumento(documento):
+    content = np.genfromtxt(documento, delimiter=',', dtype=int)
+    content = np.delete(content,(0), axis = 0)
+    return content
+
+def sigmoid(soma):
+    return 1 / (1 + np.exp(-soma))
+
+def sigmoidDerivada(sig):
+    return sig * (1 - sig)
+
+entradas = importDocumento('rede.csv')
+valoresSaida = importDocumento('result.csv')
+saidas = np.empty([999, 1], dtype=int)
+for i in range(999):
+    saidas[i] = valoresSaida[i]
+
+pesos0 = 2*np.random.random((7,500)) - 1
+pesos1 = 2*np.random.random((500,1)) - 1
+
+epocas = 100
+taxaAprendizagem = 0.9
+momento = 100
+
+for j in range(epocas):
+    camadaEntrada = entradas
+    somaSinapse0 = np.dot(camadaEntrada, pesos0)
+    camadaOculta = sigmoid(somaSinapse0)
+    
+    somaSinapse1 = np.dot(camadaOculta, pesos1)
+    camadaSaida = sigmoid(somaSinapse1)
+    
+    erroCamadaSaida = saidas - camadaSaida
+    mediaAbsoluta = np.mean(np.abs(erroCamadaSaida))
+    print("Erro: " + str(mediaAbsoluta))
+    
+    derivadaSaida = sigmoidDerivada(camadaSaida)
+    deltaSaida = erroCamadaSaida * derivadaSaida
+    
+    pesos1Transposta = pesos1.T
+    deltaSaidaXPeso = deltaSaida.dot(pesos1Transposta)
+    deltaCamadaOculta = deltaSaidaXPeso * sigmoidDerivada(camadaOculta)
+    
+    camadaOcultaTransposta = camadaOculta.T
+    pesosNovo1 = camadaOcultaTransposta.dot(deltaSaida)
+    pesos1 = (pesos1 * momento) + (pesosNovo1 * taxaAprendizagem)
+    
+    camadaEntradaTransposta = camadaEntrada.T
+    pesosNovo0 = camadaEntradaTransposta.dot(deltaCamadaOculta)
+    pesos0 = (pesos0 * momento) + (pesosNovo0 * taxaAprendizagem)
